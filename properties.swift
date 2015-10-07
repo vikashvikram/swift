2ce173ff5cfe99617f0fd1e3fd86c51836a3a054
Vikash Vikram <vikashvikram@gmail.com>
2015-10-07 10:17:59 +0530

    adding programs for properties

---
+++ properties.swift
@@ -0,0 +1,178 @@
+//
+//  properties.swift
+//  
+//
+//  Created by Vikash Vikram Singh on 10/6/15.
+//
+//
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+// the range represents integer values 0, 1, and 2
+rangeOfThreeItems.firstValue = 6
+// the range now represents integer values 6, 7, and 8
+
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+// this range represents integer values 0, 1, 2, and 3
+//rangeOfFourItems.firstValue = 6
+// this will report an error, even though firstValue is a variable property
+
+//When an instance of a value type is marked as a constant, so are all of its properties.
+
+//If you assign an instance of a reference type to a constant, 
+//you can still change that instance’s variable properties.
+
+
+class DataImporter {
+    /*
+    DataImporter is a class to import data from an external file.
+    The class is assumed to take a non-trivial amount of time to initialize.
+    */
+    var fileName = "data.txt"
+    // the DataImporter class would provide data importing functionality here
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+    // the DataManager class would provide data management functionality here
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// the DataImporter instance for the importer property has not yet been created
+
+print(manager.importer.fileName)
+// the DataImporter instance for the importer property has now been created
+// prints "data.txt"
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+    size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// prints "square.origin is now at (10.0, 10.0)"
+
+
+//If a computed property’s setter does not define a name for the new value to be set, 
+//a default name of newValue is used.
+struct AlternativeRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+//You can simplify the declaration of a read-only computed property by removing the get keyword and its braces:
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+// prints "the volume of fourByFiveByTwo is 40.0"
+
+
+//The StepCounter class declares a totalSteps property of type Int. 
+//This is a stored property with willSet and didSet observers.
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet {
+            print("About to set totalSteps to \(newValue)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+// About to set totalSteps to 896
+// Added 536 steps
+
+//Global variables are variables that are defined outside of any function, method, closure, or type context.
+//Local variables are variables that are defined within a function, method, or closure context.
+
+//Global constants and variables are always computed lazily, in a similar manner to Lazy Stored Properties. 
+//Unlike lazy stored properties, global constants and variables do not need to be marked with the lazy modifier.
+//Local constants and variables are never computed lazily.
+
+//Stored type properties can be variables or constants.
+//Computed type properties are always declared as variable properties, 
+//in the same way as computed instance properties.
+
+struct SomeStructure {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 1
+    }
+}
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 6
+    }
+}
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+
+print(SomeStructure.storedTypeProperty)
+// prints "Some value."
+SomeStructure.storedTypeProperty = "Another value."
+print(SomeStructure.storedTypeProperty)
+// prints "Another value."
+print(SomeEnumeration.computedTypeProperty)
+// prints "6"
+print(SomeClass.computedTypeProperty)
+// prints "27"
