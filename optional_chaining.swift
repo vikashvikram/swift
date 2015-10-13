fa75efe851d9d9fedc42def54d6c0b5e7eb9614e
Vikash Vikram <vikashvikram@gmail.com>
2015-10-13 10:50:26 +0530

    adding more swift programs

---
+++ optional_chaining.swift
@@ -0,0 +1,49 @@
+class Person1 {
+    var residence: Residence1?
+}
+
+class Residence1 {
+    var numberOfRooms = 1
+}
+
+let john = Person1()
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// prints "Unable to retrieve the number of rooms."
+
+john.residence = Residence1()
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// prints "John's residence has 1 room(s)."
+
+class Person {
+    var residence: Residence?
+}
+
+
+class Residence {
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
